91a65e2295c1f48fd1456fa8991eadf718fc0cbc
DElAmin <delamin13@gmail.com>
2021-01-27 14:56:40 +0100

    Creat a socket when client connects

--- _realtime_chat/server.js
+++ _realtime_chat/server.js
@@ -1,12 +1,19 @@
 const path =require('path');
 const http =require('http');
 const express = require('express');
+const socketio =require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Set when client connects
+io.on('connection', socket => {
+    console.log('new WS Connection...');
+})
 
 const PORT = 3000 || process.env.PORT;
 
