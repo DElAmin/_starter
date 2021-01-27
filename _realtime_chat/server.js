6491f92324602df0cb2a032dbd9746e78dd41e37
DElAmin <delamin13@gmail.com>
2021-01-27 14:32:05 +0100

    Set up a server to handle socket

--- _realtime_chat/server.js
+++ _realtime_chat/server.js
@@ -1,11 +1,16 @@
 const path =require('path');
+const http =require('http');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//Set up a server to handle socket
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
